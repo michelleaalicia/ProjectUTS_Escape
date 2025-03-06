97194f5ba0fab0db22510ebb2881bc2d6b42c9eb
RastyMaharaniSavitri <s160423143@student.ubaya.ac.id>
2025-03-06 11:57:27 +0700

    formCheckOut

--- src/projectuts/ProjectUTS.java
+++ src/projectuts/ProjectUTS.java
@@ -15,7 +15,8 @@ public class ProjectUTS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hallo");
+        
+        
     }
     
 }
