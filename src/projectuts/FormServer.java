317a18d253579d06aad3afb79f35cc403c48cf6a
JordanTanadi <jordantanadi1503@gmail.com>
2025-04-02 16:10:44 +0700

    ....

---
+++ src/projectuts/FormServer.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package projectuts;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author asus
+ */
+public class FormServer extends javax.swing.JFrame implements Runnable {
+
+    String chatClient,chatServer ="";
+    Socket incoming;
+    ServerSocket s ;
+    Thread t;
+    public FormServer() throws IOException {
+        initComponents();
+        
+         try
+        {
+         s= new ServerSocket(6000);
+         incoming = s.accept();
+         if(t == null)
+         {
+             t= new Thread(this,"Simple Chat");
+             t.start();
+         }
+        }
+        catch(Exception ex)
+         {
+             Logger.getLogger(FormServer.class.getName()).log(Level.SEVERE,null,ex);
+         }
+    }
+    
+     public void getChat() throws IOException
+    {
+    
+        BufferedReader chatFromClient = new BufferedReader (new InputStreamReader(incoming.getInputStream()));
+        chatClient = chatFromClient.readLine();
+        /*txtChat.append("Client : " + chatClient + "\n");*/
+    }
+    
+    @Override
+    public void run()
+    {
+        try
+        {
+            while(true)
+            {
+                getChat();
+            }
+        }
+        catch(Exception ex )
+        {
+            Logger.getLogger(FormServer.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FormServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FormServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FormServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormServer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new FormServer().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(FormServer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
